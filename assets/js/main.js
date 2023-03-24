0022afe093a98db44491c83c4dd6bf7475caaf1b
Naiem-Devs <dnsnaiem@gmail.com>
2023-03-25 00:05:15 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+  $('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+  
+ // menu 
+  $('.add_cart_btn').click( function (){ 
+    $('.add_cart').toggleClass('siteBar');   
+  }); 
+
+
+  $('select').niceSelect();
+ 
+$('.product-img--main')
+        // tile mouse actions
+        .on('mouseover', function(){
+          $(this).children('.product-img--main__image').css({'transform': 'scale('+ $(this).attr('data-scale') +')'});
+        })
+        .on('mouseout', function(){
+          $(this).children('.product-img--main__image').css({'transform': 'scale(1)'});
+        })
+        .on('mousemove', function(e){
+          $(this).children('.product-img--main__image').css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +'%'});
+        })
+        // tiles set up
+        .each(function(){
+          $(this)
+            // add a image container
+            .append('<div class="product-img--main__image"></div>')
+            // set up a background image for each tile based on data-image attribute
+            .children('.product-img--main__image').css({'background-image': 'url('+ $(this).attr('data-image') +')'});
+        });
+
+        $(".coupon_btn").click(function() {
+          $(".pay_title").toggleClass("active")
+      })
+
+})(jQuery);
